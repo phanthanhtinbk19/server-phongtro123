b3c0300d2fc9b37509c70e91d368e03585f55253
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2023-05-22 21:57:14 +0700

    update

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,20 +1,28 @@
-const {Sequelize} = require("sequelize");
-require("dotenv").config();
-
-// Option 2: Passing parameters separately (other dialects)
+// Option 2: Passing parameters separately (sqlite)
+import {Sequelize} from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 const sequelize = new Sequelize(
-	process.env.DB_DATABASE_NAME,
+	process.env.DB_NAME,
 	process.env.DB_USERNAME,
 	process.env.DB_PASSWORD,
 	{
 		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
 		dialect: process.env.DB_DIALECT,
+		port: process.env.DB_PORT,
 		logging: false,
-		query: {
-			raw: true,
+		pool: {
+			max: 20,
+			min: 0,
+			acquire: 60000,
+			idle: 10000,
 		},
-		timezone: "+07:00",
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	}
 );
 
@@ -26,4 +34,5 @@ let connectDB = async () => {
 		console.error("Unable to connect to the database:", error);
 	}
 };
-module.exports = connectDB;
+
+export default connectDB;
--- src/services/insert.js
+++ src/services/insert.js
@@ -28,6 +28,37 @@ const dataBody = [
 	},
 ];
 
+const categories = [
+	{
+		code: "CTPT",
+		value: "Cho thuê phòng trọ",
+		header: "Cho Thuê Phòng Trọ, Giá Rẻ, Tiện Nghi, Mới Nhất 2022",
+		subheader:
+			"Cho thuê phòng trọ - Kênh thông tin số 1 về phòng trọ giá rẻ, phòng trọ sinh viên, phòng trọ cao cấp mới nhất năm 2022. Tất cả nhà trọ cho thuê giá tốt nhất tại Việt Nam.",
+	},
+	{
+		code: "CTMB",
+		value: "Cho thuê mặt bằng",
+		header:
+			"Cho Thuê Mặt Bằng, Cho Thuê Văn Phòng, Cửa Hàng, Kiot, Mới Nhất 2022",
+		subheader:
+			"Cho thuê mặt bằng - Kênh đăng tin cho thuê mặt bằng, cho thuê cửa hàng, cho thuê kiot số 1: giá rẻ, mặt tiền, khu đông dân cư, phù hợp kinh doanh.",
+	},
+	{
+		code: "CTCH",
+		value: "Cho thuê căn hộ",
+		header: "Cho Thuê Căn Hộ Chung Cư, Giá Rẻ, Mới Nhất 2022",
+		subheader:
+			"Cho thuê căn hộ - Kênh đăng tin cho thuê căn hộ số 1: giá rẻ, chính chủ, đầy đủ tiện nghi. Cho thuê chung cư với nhiều mức giá, diện tích cho thuê khác nhau.",
+	},
+	{
+		code: "NCT",
+		value: "Nhà cho thuê",
+		header: "Cho Thuê Nhà Nguyên Căn, Giá Rẻ, Chính Chủ, Mới Nhất 2022",
+		subheader:
+			"Cho thuê nhà nguyên căn - Kênh đăng tin cho thuê nhà số 1: giá rẻ, chính chủ, miễn trung gian, đầy đủ tiện nghi, mức giá, diện tích cho thuê khác nhau.",
+	},
+];
 const hashPassword = (password) =>
 	bcrypt.hashSync(password, bcrypt.genSaltSync(12));
 
@@ -36,110 +67,125 @@ export const insertService = () =>
 		try {
 			const provinceCodes = [];
 			const labelCodes = [];
-			dataBody.forEach((cate) => {
-				cate.body.forEach(async (item) => {
-					let postId = v4();
-					let labelCode = generateCode(item?.header?.class?.classType).trim();
-					labelCodes?.every((item) => item?.code !== labelCode) &&
-						labelCodes.push({
-							code: labelCode,
-							value: item?.header?.class?.classType?.trim(),
-						});
-					let provinceCode = generateCode(
-						item?.header?.address?.split(",")?.slice(-1)[0]
-					).trim();
-					provinceCodes?.every((item) => item?.code !== provinceCode) &&
-						provinceCodes.push({
-							code: provinceCode,
-							value: item?.header?.address?.split(",")?.slice(-1)[0].trim(),
-						});
-					let attributesId = v4();
-					let userId = v4();
-					let imagesId = v4();
-					let overviewId = v4();
-					let desc = JSON.stringify(item?.mainContent?.content);
-					let currentArea = getNumberFromString(
-						item?.header?.attributes?.acreage
-					);
-					let currentPrice = getNumberFromString(
-						item?.header?.attributes?.price
-					);
-					await db.Post.create({
-						id: postId,
-						title: item?.header?.title,
-						star: +item?.header?.star,
-						labelCode,
-						address: item?.header?.address,
-						attributesId,
-						categoryCode: cate.code,
-						description: desc,
-						userId,
-						overviewId,
-						imagesId,
-						areaCode: dataArea.find(
-							(area) => area.max > currentArea && area.min <= currentArea
-						)?.code,
-						priceCode: dataPrice.find(
-							(area) => area.max > currentPrice && area.min <= currentPrice
-						)?.code,
-						provinceCode,
-						priceNumber: getNumberFromStringV2(item?.header?.attributes?.price),
-						areaNumber: getNumberFromStringV2(
-							item?.header?.attributes?.acreage
-						),
-					});
-					await db.Attribute.create({
-						id: attributesId,
-						price: item?.header?.attributes?.price,
-						acreage: item?.header?.attributes?.acreage,
-						published: item?.header?.attributes?.published,
-						hashtag: item?.header?.attributes?.hashtag,
-					});
-					await db.Image.create({
-						id: imagesId,
-						image: JSON.stringify(item?.images),
-					});
-					await db.Overview.create({
-						id: overviewId,
-						code: item?.overview?.content.find((i) => i.name === "Mã tin:")
-							?.content,
-						area: item?.overview?.content.find((i) => i.name === "Khu vực")
-							?.content,
-						type: item?.overview?.content.find(
-							(i) => i.name === "Loại tin rao:"
-						)?.content,
-						target: item?.overview?.content.find(
-							(i) => i.name === "Đối tượng thuê:"
-						)?.content,
-						bonus: item?.overview?.content.find((i) => i.name === "Gói tin:")
-							?.content,
-						saved: false,
-						created: item?.overview?.content.find(
-							(i) => i.name === "Ngày đăng:"
-						)?.content,
-						expired: item?.overview?.content.find(
-							(i) => i.name === "Ngày hết hạn:"
-						)?.content,
-					});
-					await db.User.create({
-						id: userId,
-						name: item?.contact?.content.find((i) => i.name === "Liên hệ:")
-							?.content,
-						password: hashPassword("123456"),
-						phone: item?.contact?.content.find((i) => i.name === "Điện thoại:")
-							?.content,
-						zalo: item?.contact?.content.find((i) => i.name === "Zalo")
-							?.content,
-					});
+			await db.Category.bulkCreate(categories);
+			dataPrice.forEach(async (item, index) => {
+				await db.Price.create({
+					code: item.code,
+					value: item.value,
+					order: index + 1,
 				});
 			});
-			// console.log(provinceCodes);
-			provinceCodes?.forEach(async (item) => {
-				await db.Province.create(item);
+			dataArea.forEach(async (item, index) => {
+				await db.Area.create({
+					code: item.code,
+					value: item.value,
+					order: index + 1,
+				});
 			});
-			labelCodes?.forEach(async (item) => {
-				await db.Label.create(item);
-			});
+			// dataBody.forEach((cate) => {
+			// 	cate.body.forEach(async (item) => {
+			// 		let postId = v4();
+			// 		let labelCode = generateCode(item?.header?.class?.classType).trim();
+			// 		labelCodes?.every((item) => item?.code !== labelCode) &&
+			// 			labelCodes.push({
+			// 				code: labelCode,
+			// 				value: item?.header?.class?.classType?.trim(),
+			// 			});
+			// 		let provinceCode = generateCode(
+			// 			item?.header?.address?.split(",")?.slice(-1)[0]
+			// 		).trim();
+			// 		provinceCodes?.every((item) => item?.code !== provinceCode) &&
+			// 			provinceCodes.push({
+			// 				code: provinceCode,
+			// 				value: item?.header?.address?.split(",")?.slice(-1)[0].trim(),
+			// 			});
+			// 		let attributesId = v4();
+			// 		let userId = v4();
+			// 		let imagesId = v4();
+			// 		let overviewId = v4();
+			// 		let desc = JSON.stringify(item?.mainContent?.content);
+			// 		let currentArea = getNumberFromString(
+			// 			item?.header?.attributes?.acreage
+			// 		);
+			// 		let currentPrice = getNumberFromString(
+			// 			item?.header?.attributes?.price
+			// 		);
+			// 		await db.Post.create({
+			// 			id: postId,
+			// 			title: item?.header?.title,
+			// 			star: +item?.header?.star,
+			// 			labelCode,
+			// 			address: item?.header?.address,
+			// 			attributesId,
+			// 			categoryCode: cate.code,
+			// 			description: desc,
+			// 			userId,
+			// 			overviewId,
+			// 			imagesId,
+			// 			areaCode: dataArea.find(
+			// 				(area) => area.max > currentArea && area.min <= currentArea
+			// 			)?.code,
+			// 			priceCode: dataPrice.find(
+			// 				(area) => area.max > currentPrice && area.min <= currentPrice
+			// 			)?.code,
+			// 			provinceCode,
+			// 			priceNumber: getNumberFromStringV2(item?.header?.attributes?.price),
+			// 			areaNumber: getNumberFromStringV2(
+			// 				item?.header?.attributes?.acreage
+			// 			),
+			// 		});
+			// 		await db.Attribute.create({
+			// 			id: attributesId,
+			// 			price: item?.header?.attributes?.price,
+			// 			acreage: item?.header?.attributes?.acreage,
+			// 			published: item?.header?.attributes?.published,
+			// 			hashtag: item?.header?.attributes?.hashtag,
+			// 		});
+			// 		await db.Image.create({
+			// 			id: imagesId,
+			// 			image: JSON.stringify(item?.images),
+			// 		});
+			// 		await db.Overview.create({
+			// 			id: overviewId,
+			// 			code: item?.overview?.content.find((i) => i.name === "Mã tin:")
+			// 				?.content,
+			// 			area: item?.overview?.content.find((i) => i.name === "Khu vực")
+			// 				?.content,
+			// 			type: item?.overview?.content.find(
+			// 				(i) => i.name === "Loại tin rao:"
+			// 			)?.content,
+			// 			target: item?.overview?.content.find(
+			// 				(i) => i.name === "Đối tượng thuê:"
+			// 			)?.content,
+			// 			bonus: item?.overview?.content.find((i) => i.name === "Gói tin:")
+			// 				?.content,
+			// 			saved: false,
+			// 			created: item?.overview?.content.find(
+			// 				(i) => i.name === "Ngày đăng:"
+			// 			)?.content,
+			// 			expired: item?.overview?.content.find(
+			// 				(i) => i.name === "Ngày hết hạn:"
+			// 			)?.content,
+			// 		});
+			// 		await db.User.create({
+			// 			id: userId,
+			// 			name: item?.contact?.content.find((i) => i.name === "Liên hệ:")
+			// 				?.content,
+			// 			password: hashPassword("123456"),
+			// 			phone: item?.contact?.content.find((i) => i.name === "Điện thoại:")
+			// 				?.content,
+			// 			zalo: item?.contact?.content.find((i) => i.name === "Zalo")
+			// 				?.content,
+			// 		});
+			// 	});
+			// });
+
+			// provinceCodes?.forEach(async (item) => {
+			// 	await db.Province.create(item);
+			// });
+			// labelCodes?.forEach(async (item) => {
+			// 	await db.Label.create(item);
+			// });
 
 			resolve("Done.");
 		} catch (error) {
